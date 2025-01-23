2093f21056963e4730cab08cbd07aaf8b2197a22
bootersite <x0452231@gmail.com>
2025-01-23 08:00:30 +0000

    Update ddosattack.sh

--- ddosattack.sh
+++ ddosattack.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-Booter.Site Its a premium layer 7 service with huge power
-
+Booter.Site Its a premium layer 7 service with huge power, also its have a free hub
 this stresser can take down most of popular protections, like CloudFlare, Vercel, Akamai.
 
 STAY WITH BEST
